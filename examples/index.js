8292b226bc3037b78e9eadd1314a36538641dc29
cocoder <cocoder16@gmail.com>
2020-07-02 15:14:52 +0900

    made video player

---
+++ examples/index.js
@@ -0,0 +1,7 @@
+var a = document.querySelector('#a');
+var b = document.querySelector('#b');
+var c = document.querySelector('#c');
+
+myVideo.create(a,'../videos/demoVideo_MyPlanner.mp4', 400, 225);
+myVideo.create(b, '../videos/demoVideo_MyPlanner.mp4', 480, 270);
+myVideo.create(c, '../videos/demoVideo_MyPlanner.mp4');
