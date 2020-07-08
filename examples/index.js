c81dc2242181cdcf07e31e1b4c32c92385bc8cf1
cocoder <cocoder16@gmail.com>
2020-07-08 11:25:30 +0900

    improve css

--- examples/index.js
+++ examples/index.js
@@ -3,7 +3,7 @@ var b = document.querySelector('#b');
 var c = document.querySelector('#c');
 var d = document.querySelector('#d');
 
-myVideo.create(a,'../videos/demoVideo_MyPlanner.mp4', 400, 225);
+myVideo.create(a,'../videos/demoVideo_MyPlanner.mp4', 320, 180);
 myVideo.create(b, '../videos/demoVideo_MyPlanner.mp4', '480px', '270px');
 myVideo.create(c, '../videos/demoVideo_MyPlanner.mp4');
 myVideo.create(d, '../videos/demoVideo_MyPlanner.mp4', '100%', '56.25%');
